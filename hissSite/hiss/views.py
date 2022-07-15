263d2f763abbc765b23e4c9c76e2c15c5c92558f
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-07-15 01:20:38 -0400

    User Login, logout, and registering

--- hissSite/hiss/views.py
+++ hissSite/hiss/views.py
@@ -1,21 +1,52 @@
 import re
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import userLoggedInData
+
 
 #creating views
 
-def login(request):
-    
-    return render(request, 'userLogin.html')
-def register(request):
-    
-    return render(request, 'userRegister.html')
-
-def userLoggedIn(request):
-    context = {}
+def home(request): #handle the Logout button
     if request.method == 'POST':
-        
-        context['userName'] = request.POST['userName']
-        context['password'] = request.POST['password']
-    return render(request,'userLoggedIn.html',context)
+        auth.logout(request)
+        return redirect('login')
+    else:
+        return render(request,'home.html')
+
+def logout(request):
+    pass
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'userLogin.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request, 'Password is not the same.')
+            return redirect('register')
+    else:
+        return render(request, 'userRegister.html')
